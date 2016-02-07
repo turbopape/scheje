b49e3857858a67c8202f92ef97d89fa68859d77c
Rafik NACCACHE <rafik@tnteam.rocks>
2016-02-07 21:22:22 +0000

    Change Scheje Website to Github pages - Bumped SNAPSHOT Version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject scheje "0.2.4"
+(defproject scheje "0.2.5-SNAPSHOT"
   :description "A Scheme Interpreter on Top of Clojure"
-  :url "http://scheje.github.io"
+  :url "https://turbopape.github.io/scheje"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
